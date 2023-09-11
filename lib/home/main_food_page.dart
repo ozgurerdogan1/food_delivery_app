c89477d41c47c257eda05ad2c96c0d2e462f66da
ozgurerdogan1 <erdgn@gmail.com>
2023-09-11 21:08:59 +0300

    first init

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
